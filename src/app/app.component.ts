cacefd424c4e59c7c8696af82c6b74b7a30ac1e3
welcome-dev <lead.dev2020@gmail.com>
2021-02-09 00:37:21 +0900

    Implement check on/off with 2 boxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fruits-status';
+  fruits = [
+    {
+      name: 'Apple',
+      isChecked: false
+    },
+    {
+      name: 'Peach',
+      isChecked: false
+    },
+    {
+      name: 'Banana',
+      isChecked: false
+    },
+    {
+      name: 'Grape',
+      isChecked: true,
+    },
+    {
+      name: 'Pear',
+      isChecked: false
+    },
+    {
+      name: 'Pineapple',
+      isChecked: false
+    }
+  ];
+
+  changeStatus(index: any): void {
+    this.fruits[index].isChecked = !this.fruits[index].isChecked;
+  }
 }
